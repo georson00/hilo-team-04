d857d39627d87fcfeafaa16250bf93a7ba6b0001
Kakeru Kikuta <kk1911shoter@icloud.com>
2022-05-14 13:10:59 +0900

    define the fuctions. (from office hour )

--- game/director.py
+++ game/director.py
@@ -22,6 +22,11 @@ class Director:
             self (Director): an instance of Director.
         """
         self.is_playing = True
+        self.cards = Card()
+        self.card_current = 0
+        self.second_card = 0
+        self.point = 300
+        self.ask_guess = ""
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -30,32 +35,38 @@ class Director:
             self (Director): an instance of Director.
         """
         while self.is_playing:
-            self.get_inputs()
-            self.do_updates()
-            self.do_outputs()
+            self.get_current_card()
+            self.get_second_card()
+            self.guess_card()
+            self.get_play_again()
 
-    def get_inputs(self):
-        """Ask the user if they want to roll.
+        print("Thank you for playing.")
+    def get_current_card(self):
+        """Get use's current card
 
         Args:
             self (Director): An instance of Director.
         """
-        pass
+        self.cards.get_cards()
+        self.card_current = self.cards.current_card
+        print(f"The card is: {self.card_current}")
 
-    def do_updates(self):
+    def get_second_card(self):
         """Updates the player's score.
 
         Args:
             self (Director): An instance of Director.
         """
-        if not self.is_playing:
-            return
+        self.ask_guess = input("Higher or Lower? [h/l]")
+        self.second_card = self.cards.card_to_guess
+        print(f"The next card was: {self.second_card}")
 
-    def do_outputs(self):
+    def guess_card(self):
         """Displays the dice and the score. Also asks the player if they want to roll again. 
 
         Args:
             self (Director): An instance of Director.
         """
-        if not self.is_playing:
-            return
+        
+
+    def play_again()
